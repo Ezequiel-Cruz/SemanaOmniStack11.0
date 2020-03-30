81d47e8e4019f418e5cb98082dddb8adcda6154e
Ezequiel Teixeira Cruz <ecruz.radix@ons.org.br>
2020-03-30 16:48:20 -0300

    Delete Caso

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -23,6 +23,22 @@ export default function Profile() {
         })
     }, [ongId]);
 
+    async function handleDeleteIncident(id) {
+        try{
+            await api.delete(`incidents/${id}`, {
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+
+            setIncidents(incidents.filter(incidents => incidents.id !== id));
+
+        }
+        catch (err) {
+            alert('Erro ao deletar caso, tente novamente.');
+        }
+    }
+
     return(
         <div className="profile-container">
             <header>
@@ -40,20 +56,19 @@ export default function Profile() {
             <ul>
                 {incidents.map(incidents => (
                     <li key={incidents.id}>
-                    <strong>CASO:</strong>
-                    <p>{incidents.title}</p>
+                        <strong>CASO:</strong>
+                        <p>{incidents.title}</p>
 
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>{incidents.description}</p>
+                        <strong>DESCRIÇÃO:</strong>
+                        <p>{incidents.description}</p>
 
-                    <strong>Valor:</strong>
-                    <p>{Intl.NumberFormat('pt-BR', {  style: 'currency', currency: 'BRL'}).format(incidents.value)}</p>
+                        <strong>Valor:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', {  style: 'currency', currency: 'BRL'}).format(incidents.value)}</p>
 
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3" />
-                    </button>
-            </li>
-                    
+                        <button onClick={() => handleDeleteIncident(incidents.id)} type="button">
+                            <FiTrash2 size={20} color="#a8a8b3" />
+                        </button>
+                    </li>
                 ))}
             </ul>
         </div>
